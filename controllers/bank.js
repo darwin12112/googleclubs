8070e115f4a4c0d18cb51c88834f2bd9b4026e33
darwin12112 <70789419+darwin12112@users.noreply.github.com>
2021-08-09 11:12:40 +0530

    Update bank.js

--- controllers/bank.js
+++ controllers/bank.js
@@ -479,11 +479,11 @@ exports.getResponseRecharge = async (req, res, next) => {
       await user.save();
       let bonus1=0;
        if (recharge.money >= 100) bonus1 = 50;
-      else if (recharge.money >= 200) bonus1 = 100;
-      else if (recharge.money >= 500) bonus1 = 150;
+      else if (recharge.money >= 200) bonus1 = 230;
+      else if (recharge.money >= 500) bonus1 = 300;
       else if (recharge.money >= 1500) bonus1 = 300;
-      else if (recharge.money >= 5000) bonus1 = 1000;
-      else if (recharge.money >= 10000) bonus1 = 2000;
+      else if (recharge.money >= 5000) bonus1 = 3000;
+      else if (recharge.money >= 10000) bonus1 = 5000;
       else if (recharge.money >= 20000) bonus1 = 5000;
       else if (recharge.money >= 50000) bonus1 = 10000;
       if (user.refer1) {
